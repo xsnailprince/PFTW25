4fe34f0e4ac52845b77ab9f67820c8957afeaa1e
xsnailprince <jgroom@mcad.edu>
2025-02-13 15:56:11 -0800

    adding week 4 exercise
    DOM

---
+++ Week_4/DOM/exercise1.js
@@ -0,0 +1,15 @@
+// select empty div and assign to a variable
+const myEmptyDiv = document.querySelector('#myEmptyDiv')
+;
+// put a h1 inside of it
+// create a new element
+const heading = document.createElement('h1');
+heading.innerHTML = 'Hello class! It is a snowy day!';
+heading.style.cursor = 'pointer';
+heading.addEventListener('click', handleHeadingClick)
+myEmptyDiv.appendChild(heading);
+function handleHeadingClick () {
+    console.log('Heading has been clicked');
+    document.body.style.backgroundColor = 'teal';
+    document.body.style.color = 'white';
+}
